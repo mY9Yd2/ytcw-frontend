c8d4556e481b72d0fdbfccbc2a5c7f841436c90e
Kovács József Miklós <kovacsjozsef7u@gmail.com>
2025-08-28 20:19:40 +0200

    Default to sddefault if thumbnail does not exist

--- src/app/services/video/video.ts
+++ src/app/services/video/video.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { PaginationResponse } from '../../interfaces/pagination';
 import { VideoResponse } from '../../interfaces/video/video';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -15,6 +15,14 @@ export class VideoService {
   getVideos(page: number = 1): Observable<PaginationResponse<VideoResponse>> {
     return this.http.get<PaginationResponse<VideoResponse>>(`${this.apiUrl}/api/v1/videos?page=${page}`, {
       mode: 'cors'
-    })
+    }).pipe( // Default to sddefault if thumbnail does not exist.
+      map(response => {
+        response.data = response.data.map(video => ({
+          ...video,
+          thumbnail: video.thumbnail && video.thumbnail.trim() !== '' ? video.thumbnail : 'sddefault.webp'
+        }));
+        return response;
+      })
+    );
   }
 }
